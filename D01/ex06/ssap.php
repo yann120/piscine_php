23595beb4a0ee7f1bfcb71afb80cabf0630e78ce
Yann PETITJEAN <ypetitje@e1r8p3.42.fr>
2019-03-21 18:54:27 +0100

    better syntax ex06

--- D01/ex06/ssap.php
+++ D01/ex06/ssap.php
@@ -4,20 +4,20 @@
 	{
 		$temps = explode(" ", $str);
 		$result = [];
-		foreach ($temps as $temp) {
+		foreach ($temps as $temp) 
+		{
 			if (!empty($temp))
 				array_push($result, $temp);
 		}
 		return($result);
 	}
-
 	$result = [];
 	$temp = [];
-	foreach ($argv as $value) {
+	foreach ($argv as $value) 
+	{
 		$temp = ft_split($value);
 		foreach ($temp as $tempvalue)
 			array_push($result, $tempvalue);
-		
 	}
 	array_shift($result);
 	sort($result);
