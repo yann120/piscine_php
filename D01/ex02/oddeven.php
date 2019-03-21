e8241d1793f11050f66110cd7e051957da4b0ecf
Yann PETITJEAN <ypetitje@e1r8p3.42.fr>
2019-03-21 17:11:46 +0100

    ex02 done

---
+++ D01/ex02/oddeven.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+	while(1)
+	{
+		echo "Entrez un nombre: ";
+		$entry = fgets(STDIN);
+
+		$entry = trim($entry);
+
+		if (is_numeric($entry))
+		{
+			if ($entry % 2 == 0)
+				echo "Le chiffre $entry est Pair\n";
+			else
+				echo "Le chiffre $entry est Impair\n";
+		}
+		else
+		{
+			if (feof(STDIN))
+			{
+				echo "\n";
+				exit();
+			}
+			else
+				echo "'$entry' n'est pas un chiffre\n";
+		}
+	}
+?>
