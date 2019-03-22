fc1bd9f61580c60b8bb34c6795aedab9b327a874
Yann <yann120@hotmail.fr>
2019-03-22 17:52:10 +0100

    ex09 done but not sure if correct with text not treated after ##

--- D01/ex09/ssap2.php
+++ D01/ex09/ssap2.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-	function ft_split($str)
+	function	ft_split($str)
 	{
 		$temps = explode(" ", $str);
 		$result = [];
@@ -11,16 +11,54 @@
 		}
 		return($result);
 	}
-	$result = [];
-	$temp = [];
-	foreach ($argv as $value) 
+	function	echo_alpha(&$tab)
 	{
-		$temp = ft_split($value);
-		foreach ($temp as $tempvalue)
-			array_push($result, $tempvalue);
+		$to_print = [];
+		foreach ($tab as $i => $value) 
+		{
+			if (ctype_alpha($value[0]))
+			{
+				array_push($to_print, $value);
+				unset($tab[$i]);
+			}
+		}
+		natcasesort($to_print);
+		foreach ($to_print as $value) 
+			echo $value."\n";
 	}
-	array_shift($result);
-	sort($result);
-	foreach ($result as $value)
-		echo "$value\n";
+
+	function	echo_num(&$tab)
+	{
+		$to_print = [];
+		foreach ($tab as  $i => $value) 
+		{
+			if (ctype_digit($value[0]))
+			{
+				array_push($to_print, $value);
+				unset($tab[$i]);
+			}
+		}
+		sort($to_print);
+		foreach ($to_print as $value) 
+			echo $value."\n";
+	}
+
+	if ($argc != 1)
+	{
+		$result = [];
+		$temp = [];
+		foreach ($argv as $value) 
+		{
+			$temp = ft_split($value);
+			foreach ($temp as $tempvalue)
+				array_push($result, $tempvalue);
+		}
+		array_shift($result);
+
+		echo_alpha($result);
+		echo_num($result);
+		sort($result);
+		foreach ($result as $value) 
+			echo $value."\n";
+	}
 ?>
