121d38b07619b9e199328c4b4d570748d9a60149
Yann PETITJEAN <ypetitje@e1r8p3.42.fr>
2019-03-25 18:53:06 +0100

    who working better but still one day before official who

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -3,12 +3,11 @@
     $fd = fopen("/var/run/utmpx", "r");
     $line = fread($fd, 1256);
     date_default_timezone_set('Europe/Paris');
-    setlocale(LC_ALL, 'fr_FR');
+    setlocale(LC_TIME, 'fr_FR', 'fra');
     // a256 pour avoir une string de longueur max 255 charactere (taille d'un user) +  \0
     while($line = fread($fd, 628))
     {
         $val = (unpack("a256username/a4id/a32devicename/ipid/itypeentry/itime", $line));
         echo $val['username']." ".$val['devicename']."  ".strftime("%a %d %H:%M", $val['time'])."\n";
-
     }
 ?>
