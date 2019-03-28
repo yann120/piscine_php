ff5595c7c011624d6fc5c5bdab995ef3fdd2b41b
Yann PETITJEAN <ypetitje@e1r8p3.42.fr>
2019-03-28 19:10:45 +0100

    D05 started

---
+++ D05/ex01/ex01.sql
@@ -0,0 +1,6 @@
+CREATE TABLE ft_table (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    login VARCHAR(8) NOT NULL DEFAULT 'toto',
+    groupe ENUM('staff', 'student', 'other') NOT NULL,
+    date_de_creation DATE NOT NULL
+);
